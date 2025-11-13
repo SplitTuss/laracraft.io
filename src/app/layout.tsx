6ef9d8e6d29386dd748f4a9db6e4c7291cf3f8f3
Lara <lolaviras@gmail.com>
2025-11-13 16:15:10 -0700

    added GoogleAnalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId="G-Y3R6E2K1Y8" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
