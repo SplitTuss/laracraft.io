5fa7e3af00d1d2c8a31ebe0aafe7f5b348d8a223
Lara <lolaviras@gmail.com>
2025-11-14 11:08:15 -0700

    added Button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/imported/Button';
 import Image from 'next/image';
 
 export default function Home() {
@@ -14,7 +15,8 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, bitte halt dein maul.
+            To get started,
+            <Button>bitte halt dein maul.</Button>
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{' '}
