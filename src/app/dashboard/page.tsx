44a9d68f885aa474d9ea41636232dc4a3fc4015a
Lara <lolaviras@gmail.com>
2025-11-17 08:20:12 -0700

    added basic routing between login, signup, dashboard and homepage

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Dashboard() {
+  return <h1 className="flex justify-center">dashboard coming soon...</h1>;
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,3 @@
+export default function Login() {
+  return <h1 className="flex justify-center">login coming soon...</h1>;
+}
