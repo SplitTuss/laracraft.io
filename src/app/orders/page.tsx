ec73fcc96d5e3f88b753c4293c4306a303677468
Lara <lolaviras@gmail.com>
2025-11-19 16:39:25 -0700

    used api on orders page

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,19 +1,51 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuth } from '@/client-auth/authContext';
 
 export default function Orders() {
-  const { session, loading } = useAuth();
+  const [orders, setOrders] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const { session, loading: authLoading } = useAuth();
   const router = useRouter();
 
   useEffect(() => {
     console.log('order page', { session });
-    if (!loading && session === null) {
+    if (!authLoading && session === null) {
       router.push('/signin');
     }
-  }, [session, loading, router]);
+  }, [session, authLoading, router]);
 
-  return <div>hallo</div>;
+  const handleLoadOrders = async () => {
+    setLoading(true);
+
+    const response = await fetch('/api/orders');
+    const data = await response.json();
+    setOrders(data);
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (authLoading || !session) return;
+
+    handleLoadOrders();
+  }, [authLoading, session]);
+
+  if (authLoading || loading) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <div>
+      {orders.map((order) => (
+        <div key={order.id}>
+          items: {order.items}
+          total: {order.total}
+        </div>
+      ))}
+    </div>
+  );
 }
