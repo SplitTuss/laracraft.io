125451f93b140afb2804fe97e4521f61cb71a423
Lara <lolaviras@gmail.com>
2025-11-19 14:18:28 -0700

    added input component

---
+++ src/components/shadcn/Input.tsx
@@ -0,0 +1,30 @@
+import { cn } from '@/styleUtils';
+
+function Input({ children, className, type, ...props }: React.ComponentProps<'input'>) {
+  const renderInput = () => (
+    <input
+      type={type}
+      data-slot="input"
+      className={cn(
+        'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+        'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
+        'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+        className,
+      )}
+      {...props}
+    />
+  );
+
+  if (!children) {
+    return renderInput();
+  }
+
+  return (
+    <div className="relative">
+      {renderInput()}
+      <div className="absolute top-0 right-0">{children}</div>
+    </div>
+  );
+}
+
+export { Input };
