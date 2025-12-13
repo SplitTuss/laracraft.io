b4597d7773bd7fb438efed75c01d07be94660a8d
Lara <lolaviras@gmail.com>
2025-12-13 06:15:27 -0700

    rearranged header buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,23 +4,25 @@ import { Button } from './shadcn/Button';
 
 export default function Header() {
   return (
-    <div className="bg-accent">
+    <div className="bg-accent p-2">
       <h1 className="flex justify-center text-2xl text-primary bg-accent">
         welcome to laracraft.io
       </h1>
-      <Button className="hover:cursor-pointer">
-        <ShoppingCartIcon />
-      </Button>
-      <Button className="hover:cursor-pointer">
-        <Link href="/profile" className="underline hover:text-primary">
-          <UserIcon />
-        </Link>
-      </Button>
-      <Button className="hover:cursor-pointer">
-        <Link href="/orders" className="underline hover:text-primary">
-          <BookTextIcon />
-        </Link>
-      </Button>
+      <div className="flex flex-row gap-2 justify-end">
+        <Button className="hover:cursor-pointer">
+          <Link href="/profile" className="underline hover:text-primary">
+            <UserIcon />
+          </Link>
+        </Button>
+        <Button className="hover:cursor-pointer">
+          <Link href="/orders" className="underline hover:text-primary">
+            <BookTextIcon />
+          </Link>
+        </Button>
+        <Button className="hover:cursor-pointer">
+          <ShoppingCartIcon />
+        </Button>
+      </div>
     </div>
   );
 }
