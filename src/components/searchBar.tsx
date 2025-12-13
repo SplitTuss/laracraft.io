3171289e06f3a8bfc435b94033ac33bb15759c4f
Lara <lolaviras@gmail.com>
2025-12-13 06:16:02 -0700

    added searchBar component

---
+++ src/components/searchBar.tsx
@@ -0,0 +1,16 @@
+import { Input } from './shadcn/Input';
+import { SearchIcon } from 'lucide-react';
+
+interface SearchBarProps {
+  onSearchChange: (value: string) => void;
+}
+
+export const SearchBar = ({ onSearchChange }: SearchBarProps) => {
+  return (
+    <div className="max-w-md mx-auto mb-2">
+      <Input placeholder="search items..." onChange={(e) => onSearchChange(e.target.value)}>
+        <SearchIcon className="text-muted-foreground mt-2 mr-2" size={20} />
+      </Input>
+    </div>
+  );
+};
