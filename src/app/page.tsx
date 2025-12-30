675ede3c46de38cebf56081617efd4bd3c4370f7
Lara <lolaviras@gmail.com>
2025-12-30 13:06:48 -0700

    used actual photos/items and added description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,103 @@ export default function Home() {
       <div className="mt-2">
         <SearchBar onSearchChange={setSearchInput} />
       </div>
-      <ul>
-        <ProductCard price={25} title="x" description="hallooooo" />
+      <ul className="grid grid-cols-5">
+        <ProductCard
+          image={{
+            imageSource:
+              'https://s3.us-east-1.amazonaws.com/laracraft.io/crochetItems/DogPillow2.jpg',
+            alt: 'Dog Pillow',
+          }}
+          price={25}
+          title="Dog Pillow"
+          description="crochet dog pillow made with chenille yarn"
+        />
 
-        <ProductCard price={20} title="y" description="hier" />
+        <ProductCard
+          image={{
+            imageSource:
+              'https://s3.us-east-1.amazonaws.com/laracraft.io/crochetItems/GreenPotato.jpg',
+            alt: 'Green Potato',
+          }}
+          price={25}
+          title="Green Potato"
+          description="crochet potato made with chenille yarn"
+        />
 
-        <ProductCard price={15} title="z" description="sup" />
+        <ProductCard
+          image={{
+            imageSource:
+              'https://s3.us-east-1.amazonaws.com/laracraft.io/crochetItems/YellowPotato.jpg ',
+            alt: 'Yellow Potato',
+          }}
+          price={25}
+          title="Yellow Potato"
+          description="crochet potato made with chenille yarn"
+        />
+
+        <ProductCard
+          image={{
+            imageSource: 'https://s3.us-east-1.amazonaws.com/laracraft.io/crochetItems/Heart.jpg',
+            alt: 'Heart',
+          }}
+          price={25}
+          title="Heart"
+          description="crochet heart made with chenille yarn"
+        />
+
+        <ProductCard
+          image={{
+            imageSource:
+              'https://s3.us-east-1.amazonaws.com/laracraft.io/crochetItems/PinkBunny.jpg',
+            alt: 'Pink Bunny',
+          }}
+          price={25}
+          title="Pink Bunny"
+          description="crochet bunny made with chenille yarn"
+        />
+
+        <ProductCard
+          image={{
+            imageSource:
+              'https://s3.us-east-1.amazonaws.com/laracraft.io/crochetItems/PinkSeal.jpg',
+            alt: 'Pink Seal',
+          }}
+          price={25}
+          title="Pink Seal"
+          description="crochet seal made with chenille yarn"
+        />
+
+        <ProductCard
+          image={{
+            imageSource:
+              'https://s3.us-east-1.amazonaws.com/laracraft.io/crochetItems/SmallPinkBunny.jpg',
+            alt: 'Small Pink Bunny',
+          }}
+          price={25}
+          title="Small Pink Bunny"
+          description="crochet bunny"
+        />
+
+        <ProductCard
+          image={{
+            imageSource: 'https://s3.us-east-1.amazonaws.com/laracraft.io/crochetItems/TinyHat.jpg',
+            alt: 'Tiny Hat',
+          }}
+          price={25}
+          title="Tiny Hat"
+          description="crochet hat with faux fur brim"
+        />
+
+        <ProductCard
+          image={{
+            imageSource:
+              'https://s3.us-east-1.amazonaws.com/laracraft.io/crochetItems/YellowBunny.jpg',
+            alt: 'Yellow Bunny',
+          }}
+          price={25}
+          title="Yellow Bunny"
+          description="crochet bunny made with chenille yarn"
+        />
       </ul>
     </>
   );
--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -2,24 +2,24 @@ import { PlusIcon } from 'lucide-react';
 import { Button } from './shadcn/Button';
 
 interface ProductProps {
-  //image: [src: imageSource, alt: alt]
+  image: { imageSource: string; alt: string };
   title: string;
   price: number;
   description: string;
 }
 
-export default function ProductCard({ title, price, description }: ProductProps) {
+export default function ProductCard({
+  image: { imageSource, alt },
+  title,
+  price,
+  description,
+}: ProductProps) {
   return (
     <li className="mb-2">
       <div className="flex flex-col items-center gap-4 border-2 rounded-xl border-accent">
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src="https://s3.us-east-1.amazonaws.com/laracraft.io/jamaica-leaf-blanket/finishedBlanket.jpeg"
-          alt="jamaica flag with leaf"
-          height="auto"
-          width={200}
-        />
-        <div className="flex flex-row gap-4">
+        <img src={imageSource} alt={alt} height="auto" width={200} />
+        <div className="flex flex-row gap-4 text-xl">
           <div className="font-bold">{title}</div>
           <div className="">{price}</div>
         </div>
