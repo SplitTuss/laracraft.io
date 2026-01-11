e1ce6128e818dc5b762d78f963d776343ab64c73
Lara <lolaviras@gmail.com>
2026-01-11 13:04:25 -0700

    added and used select component

--- src/components/cartItem.tsx
+++ src/components/cartItem.tsx
@@ -2,6 +2,13 @@ import { useState, useEffect } from 'react';
 import { Button } from '@/components/shadcn/Button';
 import { useCart, CartItem } from '@/cartContext';
 import { ProductData } from '@/app/page';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/shadcn/Select';
 
 interface CartItemProps {
   item: CartItem;
@@ -34,18 +41,44 @@ export default function CartItemComponent({ item }: CartItemProps) {
 
   return (
     <div className="border-2 rounded-lg">
-      <div className="flex justify-center font-bold text-lg mb-2">{product.title}</div>
+      <div className="flex justify-center font-bold text-lg">{product.title}</div>
       <div className="flex flex-row items-center justify-between m-2">
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={product.imageUrl} alt={product.title} height="auto" width={50} className="mt-4" />
-        <div className="flex flex-row justify-center gap-2 mb-2">
+        <img src={product.imageUrl} alt={product.title} height="auto" width={50} />
+        <div className="flex flex-row justify-center items-center gap-2">
           <Button
             size="icon-sm"
             onClick={() => updateCart({ productId: item.productId, quantity: item.quantity - 1 })}
           >
             -
           </Button>
-          <div>{item.quantity}</div>
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder={item.quantity} />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="1">1</SelectItem>
+              <SelectItem value="2">2</SelectItem>
+              <SelectItem value="3">3</SelectItem>
+              <SelectItem value="4">4</SelectItem>
+              <SelectItem value="5">5</SelectItem>
+              <SelectItem value="6">6</SelectItem>
+              <SelectItem value="7">7</SelectItem>
+              <SelectItem value="8">8</SelectItem>
+              <SelectItem value="9">9</SelectItem>
+              <SelectItem value="10">10</SelectItem>
+              <SelectItem value="11">11</SelectItem>
+              <SelectItem value="12">12</SelectItem>
+              <SelectItem value="13">13</SelectItem>
+              <SelectItem value="14">14</SelectItem>
+              <SelectItem value="15">15</SelectItem>
+              <SelectItem value="16">16</SelectItem>
+              <SelectItem value="17">17</SelectItem>
+              <SelectItem value="18">18</SelectItem>
+              <SelectItem value="19">19</SelectItem>
+              <SelectItem value="20">20</SelectItem>
+            </SelectContent>
+          </Select>
           <Button
             size="icon-sm"
             onClick={() => updateCart({ productId: item.productId, quantity: item.quantity + 1 })}
