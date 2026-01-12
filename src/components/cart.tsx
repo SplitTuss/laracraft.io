92b0599b95b3302611fbc5bf0c91f4a3327f120a
Lara <lolaviras@gmail.com>
2026-01-11 18:03:05 -0700

    added logic for having to be signed in when trying to place order

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,5 +1,5 @@
 import { ShoppingCartIcon } from 'lucide-react';
-
+import { useRouter } from 'next/navigation';
 import { Button } from './shadcn/Button';
 import {
   Dialog,
@@ -10,10 +10,34 @@ import {
   DialogTrigger,
 } from '@/components/shadcn/Dialog';
 import { useCart } from '@/cartContext';
-import CartItem from './cartItem';
+import { useState } from 'react';
+import { supabase } from '@/client-auth/supabase-client';
+import { useAuth } from '@/client-auth/authContext';
+import CartItemComponent from './cartItem';
 
 export default function Cart() {
   const { cart } = useCart();
+  const { session, loading: authLoading } = useAuth();
+  const router = useRouter();
+  const [newOrder, setNewOrder] = useState({
+    quantity: '',
+    productId: '',
+  });
+
+  // i want to be able to see the cart.quantity for each item.
+
+  //OrderID is an object with userID, orderId number and CartItem: item id, item total
+  const AddOrderFunction = async () => {
+    const { error } = await supabase.from('order_products').insert(newOrder);
+    if (!authLoading && session === null) {
+      router.push('/signin');
+    }
+    if (error) {
+      console.log('an error occured adding the order:', error.message);
+    }
+    setNewOrder({ quantity: '', productId: '4' });
+    console.log(newOrder);
+  };
 
   return (
     <Dialog>
@@ -30,11 +54,13 @@ export default function Cart() {
         </DialogHeader>
 
         {cart.map((item) => (
-          <CartItem key={item.productId} item={item} />
+          <CartItemComponent key={item.productId} item={item} />
         ))}
 
         <DialogFooter>
-          <Button type="submit">checkout</Button>
+          <Button onClick={AddOrderFunction} type="submit">
+            checkout
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
