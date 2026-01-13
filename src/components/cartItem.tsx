2d3a68074b6f2c3e174f410b119ef1a4f21ff139
Lara <lolaviras@gmail.com>
2026-01-13 10:17:57 -0700

    refactored select items

--- src/components/cartItem.tsx
+++ src/components/cartItem.tsx
@@ -54,31 +54,20 @@ export default function CartItemComponent({ item }: CartItemProps) {
           >
             -
           </Button>
-          <Select>
+          <Select
+            onValueChange={(newValue) => {
+              updateCart({ productId: item.productId, quantity: Number(newValue) });
+            }}
+          >
             <SelectTrigger>
               <SelectValue placeholder={item.quantity} />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="1">1</SelectItem>
-              <SelectItem value="2">2</SelectItem>
-              <SelectItem value="3">3</SelectItem>
-              <SelectItem value="4">4</SelectItem>
-              <SelectItem value="5">5</SelectItem>
-              <SelectItem value="6">6</SelectItem>
-              <SelectItem value="7">7</SelectItem>
-              <SelectItem value="8">8</SelectItem>
-              <SelectItem value="9">9</SelectItem>
-              <SelectItem value="10">10</SelectItem>
-              <SelectItem value="11">11</SelectItem>
-              <SelectItem value="12">12</SelectItem>
-              <SelectItem value="13">13</SelectItem>
-              <SelectItem value="14">14</SelectItem>
-              <SelectItem value="15">15</SelectItem>
-              <SelectItem value="16">16</SelectItem>
-              <SelectItem value="17">17</SelectItem>
-              <SelectItem value="18">18</SelectItem>
-              <SelectItem value="19">19</SelectItem>
-              <SelectItem value="20">20</SelectItem>
+              {[...Array(20)].map((_, index) => (
+                <SelectItem key={index} value={`${index + 1}`}>
+                  {index + 1}
+                </SelectItem>
+              ))}
             </SelectContent>
           </Select>
           <Button
