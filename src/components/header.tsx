0b32b809947fb6399a797a71406aa4e224c854ed
Lara <lolaviras@gmail.com>
2026-03-17 14:37:15 -0700

    removed orders button from header and added home link to the header image

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import { UserIcon, BookTextIcon } from 'lucide-react';
+import { UserIcon } from 'lucide-react';
 import Link from 'next/link';
 import { Button } from './shadcn/Button';
 import Cart from './cart';
@@ -6,10 +6,10 @@ import Cart from './cart';
 export default function Header() {
   return (
     <div className="flex flex-row justify-between bg-accent p-2 h-25">
-      <div>
+      <Link href="/">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/favicon.ico" alt="laracraft.io logo" className="w-20 h-20 rounded-lg" />
-      </div>
+      </Link>
 
       <div className="mt-6 text-center text-3xl text-primary">
         <Link href="/">laracraft.io</Link>
@@ -21,11 +21,7 @@ export default function Header() {
             <UserIcon />
           </Link>
         </Button>
-        <Button className="hover:cursor-pointer" size="icon-sm">
-          <Link href="/orders" className="hover:text-primary">
-            <BookTextIcon />
-          </Link>
-        </Button>
+
         <Cart />
       </div>
     </div>
