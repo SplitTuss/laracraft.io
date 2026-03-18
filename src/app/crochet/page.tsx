320a4f0be4e23d60d7bb6af1fb618ab9937db211
Lara <lolaviras@gmail.com>
2026-03-18 13:07:46 -0700

    made crochet page

---
+++ src/app/crochet/page.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Header from '@/components/header';
+import { SearchBar } from '@/components/searchBar';
+import ProductCard from '@/components/productCard';
+
+export type ProductData = {
+  id: number;
+  imageUrl: string;
+  title: string;
+  price: number;
+  description: string;
+};
+
+export default function Crochet() {
+  const [searchInput, setSearchInput] = useState('');
+  const [products, setProducts] = useState<Array<ProductData>>([]);
+
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(searchInput.toLowerCase()),
+  );
+
+  const handleLoadProducts = async () => {
+    const result = await fetch('/api/products');
+
+    if (!result.ok) {
+      console.log('products not ok');
+      return;
+    }
+
+    const data = await result.json();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    handleLoadProducts();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <div className="m-4">
+        <SearchBar onSearchChange={setSearchInput} />
+      </div>
+      <div className="flex justify-center">
+        <ul className="grid sm:grid-cols-3 grid-cols-2 m-2">
+          {filteredProducts.map((product) => (
+            <ProductCard
+              key={product.id}
+              productId={product.id}
+              imageUrl={product.imageUrl}
+              title={product.title}
+              price={product.price}
+              description={product.description}
+            />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
