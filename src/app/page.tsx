b94e42524aacd730596a950c040e2540a82691bb
Lara <lolaviras@gmail.com>
2026-03-20 11:05:52 -0700

    updated home page with bg-images and links to both main pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,42 @@
 'use client';
-
-import { useEffect, useState } from 'react';
+import Link from 'next/link';
 import Header from '../components/header';
-import { SearchBar } from '../components/searchBar';
-import ProductCard from '@/components/productCard';
-
-export type ProductData = {
-  id: number;
-  imageUrl: string;
-  title: string;
-  price: number;
-  description: string;
-};
+import { Button } from '@/components/shadcn/Button';
 
 export default function Home() {
-  const [searchInput, setSearchInput] = useState('');
-  const [products, setProducts] = useState<Array<ProductData>>([]);
-
-  const filteredProducts = products.filter((product) =>
-    product.title.toLowerCase().includes(searchInput.toLowerCase()),
-  );
-
-  const handleLoadProducts = async () => {
-    const result = await fetch('/api/products');
-
-    if (!result.ok) {
-      console.log('products not ok');
-      return;
-    }
-
-    const data = await result.json();
-    setProducts(data);
-  };
-
-  useEffect(() => {
-    handleLoadProducts();
-  }, []);
-
   return (
     <>
       <Header />
-      <div className="m-4">
-        <SearchBar onSearchChange={setSearchInput} />
+      <div className="rounded-lg mb-2 h-[450px] sm:h-[600px] bg-[url('https://s3.us-east-1.amazonaws.com/laracraft.io/diamond-paintings/SneakyCatDac.png')] bg-cover bg-center">
+        <div className="mx-auto max-w-xs sm:max-w-2xl flex flex-col text-center justify-center">
+          <div className="font-extrabold text-primary/70 text-6xl sm:text-8xl mt-10 mb-15">
+            Welcome!
+          </div>
+          <div className="rounded-lg p-2 bg-accent-foreground/70 text-primary mb-4 mt-5 sm:mt-25">
+            Look at my{' '}
+            <Link href="/diamondArt" className="underline">
+              Diamond art
+            </Link>
+          </div>
+
+          <div>
+            <Button className="mb-4 mt-15 sm:mt-35">
+              <Link href="/profile" className="flex justify-center">
+                Profile page
+              </Link>
+            </Button>
+          </div>
+        </div>
       </div>
-      <div className="flex justify-center">
-        <ul className="grid sm:grid-cols-3 grid-cols-2 m-2">
-          {filteredProducts.map((product) => (
-            <ProductCard
-              key={product.id}
-              productId={product.id}
-              imageUrl={product.imageUrl}
-              title={product.title}
-              price={product.price}
-              description={product.description}
-            />
-          ))}
-        </ul>
+      <div className="rounded-lg mb-2 h-[450px] sm:h-[600px] bg-[url('https://s3.us-east-1.amazonaws.com/laracraft.io/crochetItems/DogPillow.jpg')] bg-cover bg-center">
+        <div className="mx-auto max-w-xs sm:max-w-2xl flex flex-col text-center justify-center">
+          <div className="rounded-lg p-2 bg-accent-foreground/40 text-primary mb-4 mt-5 sm:mt-25">
+            Buy my{' '}
+            <Link href="/crochet" className="underline">
+              Crochet stuff
+            </Link>{' '}
+          </div>
+        </div>
       </div>
     </>
   );
